74bde9e008929c88aefc65909a1de0f2becf16d6
Tmin1021 <128274812+Tmin1021@users.noreply.github.com>
2025-08-09 15:53:45 +0700

    Fix How To Play Scene

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -302,7 +302,7 @@ public class PlayerMovement : MonoBehaviour
             lastLaneY = transform.position.y;
         }
 
-        if (direction == Vector3.up && transform.position.y >= laneManager.getLastSpawnY() - 5)
+        if (direction == Vector3.up && transform.position.y >= laneManager.getLastSpawnY() - 3)
         {
             laneManager.SpawnLane();
             cameraAutoScroll.MoveUpOneLane();
